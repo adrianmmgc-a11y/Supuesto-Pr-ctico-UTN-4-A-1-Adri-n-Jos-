816b1a5ef50c189b8bcbbfbc12ad23e7f7ab3bf7
Adrián José Martínez Medina <adrianmmgc@gmail.com>
2026-04-24 08:50:30 +0100

    Se renombro la clase Maina a NumeroPrimo y se renombraron sus variablees

---
+++ NumeroPrimo.java
@@ -0,0 +1,34 @@
+package ofuscado;
+
+import java.util.Scanner;
+
+public class NumeroPrimo {
+	public static void main(String[] args) {
+		Scanner t = new Scanner(System.in);
+		int numero1, numero2;
+		boolean esPrimo;
+		System.out.print("Introduzca primer numero: ");
+		numero1 = t.nextInt();
+		System.out.print("Introduzca segundo numero: ");
+		numero2 = t.nextInt();
+		int mayor, menor;
+		if (numero1 > numero2) {
+			mayor = numero1;
+			menor = numero2;
+		} else {
+			mayor = numero2;
+			menor = numero1;
+		}
+		for (int i = menor; i <= mayor; i++) {
+			esPrimo = true;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					esPrimo = false;
+				}
+			}
+			if (esPrimo) {
+				System.out.println(i + " ");
+			}
+		}
+	}
+}
